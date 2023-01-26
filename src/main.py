8acdd98171eba7fb1b4b910541376517b037c6ff
Adhavan Sivaraj <adhavan.sivaraj16@gmail.com>
2023-01-26 16:08:20 +0530

    geo search optional

--- src/main.py
+++ src/main.py
@@ -13,26 +13,33 @@ hashtags = config['hashtags']
 tweet_limit = config['tweet_limit']
 bboxCenter = [12.971421, 77.5946]
 searchRadius = config['search_radius']
+date_since = 'since:' + config['since_date']
 fileName = "data/tweet_data" + "_" + str(date.today()) + ".csv"
+geo_search_enabled = config['geo_search_enabled']
 
 # Create a new TwitterScraper object
-tweets_search = twitter.TwitterSearchScraper(hashtags).get_items()
+search_term = hashtags + ' ' + date_since
+print(search_term)
+tweets_search = twitter.TwitterSearchScraper( search_term, maxEmptyPages=100).get_items()
 raw_tweet_list = []
 final_tweet_list = []
 
-# Limiting the tweets to the specified limit
-for count, tweet in enumerate(tweets_search):
-    if count >= tweet_limit:
-        break # reached max
-    raw_tweet_list.append(tweet)
+# # Limiting the tweets to the specified limit
+# for count, tweet in enumerate(tweets_search):
+#     if count >= tweet_limit:
+#         break # reached max
+#     raw_tweet_list.append(tweet)
+
+raw_tweet_list = list(tweets_search)
 print(hashtags + " tweets scraped")
 
-# Filtering the tweets based on the location
-for tweet in raw_tweet_list:
-    if tweet.coordinates:
-        if distance(bboxCenter, [tweet.coordinates.latitude, tweet.coordinates.longitude]).km <= searchRadius:
-            final_tweet_list.append(tweet)
-print(hashtags + " tweets filtered by location")
+# Filtering the tweets based on the loc ation
+if geo_search_enabled:
+    for tweet in raw_tweet_list:
+        if tweet.coordinates:
+            if distance(bboxCenter, [tweet.coordinates.latitude, tweet.coordinates.longitude]).km <= searchRadius:
+                final_tweet_list.append(tweet)
+    print(hashtags + " tweets filtered by location")
 
 # Open a CSV file to write the data 
 with open(fileName, mode='w', encoding="utf-8") as file:
